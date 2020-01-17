3d4c1cb6208049aa32632d8fe7563f52f51507ef
MarcBarnes <MarcusBreelandBarnes@gmail.com>
2020-01-17 11:55:16 -0500

    EntriesController

---
+++ ExpensesAPI/ExpensesAPI/Controllers/EntriesController.cs
@@ -0,0 +1,33 @@
+﻿using ExpensesAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpensesAPI.Controllers
+{
+    public class EntriesController : ApiController
+    {
+        public IHttpActionResult GetEntries()
+        {
+
+            try
+            {
+                //opens connection string, closes it when done
+                using (var context = new AppDbContext())
+                {
+                    var entries = context.Entries.ToList();
+                    return Ok(entries);
+                }
+            }
+            catch (Exception exConn)
+            {
+                return BadRequest(exConn.Message);
+                throw;
+            }
+
+        }
+    }
+}
--- ExpensesClient/src/app/app.module.ts
+++ ExpensesClient/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
